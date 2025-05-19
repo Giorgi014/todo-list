c71460e13a169a586a712af26dbe6d9f1d3c2448
Giorgi014 <giorgi.gugunava99@gmail.com>
2025-05-19 16:04:34 +0400

    some bugs are fix

--- app.js
+++ app.js
@@ -33,7 +33,9 @@ const creatTodoItem = (todo, todoIndex) => {
   const todoDiv = document.createElement("div");
   const todoText = todo.text;
   todoDiv.className = "todo";
-  todoDiv.innerHTML = `<input type="checkbox" id="${todoID}">
+  todoDiv.innerHTML = `
+                    <div id="todo">
+                <input type="checkbox" class="${todoID}">
             <label for="${todoID}" class="custom_checkbox">
                 <svg fill="transparent" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>
             </label>
@@ -43,6 +45,7 @@ const creatTodoItem = (todo, todoIndex) => {
             <button class="delete_button">
                 <svg fill="azure" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
             </button>
+            </div>
                   `;
   const deleteButton = todoDiv.querySelector(".delete_button");
   deleteButton.addEventListener("click", () => {
@@ -55,6 +58,11 @@ const creatTodoItem = (todo, todoIndex) => {
   });
   checkbox.checked = todo.completed;
   return todoDiv;
+};
+const deleteTodoItem = (todoIndex) => {
+  allTodos = allTodos.filter((_, i) => i !== todoIndex);
+  saveTodos();
+  updateTodoList();
 };
 
 const saveTodos = () => {
