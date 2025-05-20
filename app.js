5cba8acab9b56ec583a68e929354ae7108abf04e
Giorgi014 <giorgi.gugunava99@gmail.com>
2025-05-20 16:06:13 +0400

    some changes

--- app.js
+++ app.js
@@ -2,37 +2,61 @@ const FORM = document.querySelector("form");
 const TODO_INPUT = document.getElementById("todo_input");
 const TODO_LIST = document.getElementById("todo");
 
-
 const getTodoApi = async () => {
-    const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
 
-    // return await res.json();
-    console.log(await res.json());
-    
-}
+  return await res.json();
+};
 
-getTodoApi();
+const renderTodo = (apiTodoList) => {
+  // todo.map((listTodo) => {
+  //   const todoList = creatTodoItem(listTodo);
 
+  //   TODO_LIST.appendChild(todoList);
+  // });
+
+  const storageTodos = getTodos();
+
+  allTodos = [...storageTodos, ...apiTodoList].map((item) => ({
+    title: item.title,
+    // completed: item.completed,
+  }));
+
+  // apiTodoList.forEach((listTodo) => {
+  //   const objectTodo = {
+  //     title: listTodo.title,
+  //     completed: listTodo.completed,
+  //   };
+  //   allTodos = [objectTodo, ...allTodos]
+  //   allTodos.unshift(objectTodo);
+  // });
+  // saveTodos();
+  updateTodoList();
+};
 
 FORM.addEventListener("submit", (e) => {
   e.preventDefault();
-  todoAdd();
-  updateTodoList();
-  saveTodos();
+  // todoAdd();
+  saveTodos({
+    title: TODO_INPUT.value.trim(),
+    completed: false
+  });
+
+  // updateTodoList();
 });
 
-const todoAdd = () => {
-  const todoText = TODO_INPUT.value.trim();
+// const todoAdd = () => {
+//   const todoText = TODO_INPUT.value.trim();
 
-  if (todoText.length > 0) {
-    const objectTodo = {
-      text: todoText,
-      completed: false,
-    };
-    allTodos.push(objectTodo);
-    TODO_INPUT.value = "";
-  }
-};
+//   if (todoText.length > 0) {
+//     const objectTodo = {
+//       title: todoText,
+//       completed: false,
+//     };
+//     allTodos.push(objectTodo);
+//     TODO_INPUT.value = "";
+//   }
+// };
 const updateTodoList = () => {
   TODO_LIST.innerHTML = "";
   allTodos.forEach((todo, todoIndex) => {
@@ -43,11 +67,12 @@ const updateTodoList = () => {
 const creatTodoItem = (todo, todoIndex) => {
   const todoID = "todo_" + todoIndex;
   const todoDiv = document.createElement("div");
-  const todoText = todo.text;
+  // const todoText = todo.text;
+  const todoText = todo.title;
   todoDiv.className = "todo";
   todoDiv.innerHTML = `
                     <div id="todo">
-                <input type="checkbox" class="${todoID}">
+                <input type="checkbox" id="${todoID}" class="checkbox">
             <label for="${todoID}" class="custom_checkbox">
                 <svg fill="transparent" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>
             </label>
@@ -64,6 +89,7 @@ const creatTodoItem = (todo, todoIndex) => {
     deleteTodoItem(todoIndex);
   });
   const checkbox = todoDiv.querySelector("input");
+
   checkbox.addEventListener("change", () => {
     allTodos[todoIndex].completed = checkbox.checked;
     saveTodos();
@@ -77,14 +103,28 @@ const deleteTodoItem = (todoIndex) => {
   updateTodoList();
 };
 
-const saveTodos = () => {
-  const todosJson = JSON.stringify(allTodos);
+const saveTodos = (newTodo) => {
+  const storageTodos = getTodos()
+  const todosJson = JSON.stringify([newTodo, ...storageTodos]);
   localStorage.setItem("todos", todosJson);
+
+  renderHtml();
 };
 
 const getTodos = () => {
   const todos = localStorage.getItem("todos") || "[]";
   return JSON.parse(todos);
 };
-let allTodos = getTodos();
+let allTodos = [];
 updateTodoList();
+
+const renderHtml = async () => {
+  try {
+    const apiTodoList = await getTodoApi();
+    renderTodo(apiTodoList);
+  } catch (err) {
+    console.log("Error: ", err);
+  }
+};
+
+renderHtml();
